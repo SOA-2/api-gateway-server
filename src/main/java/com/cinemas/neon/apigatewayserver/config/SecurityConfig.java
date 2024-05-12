ee2802d5dc9c33138ca229229d4ae60f52650479
Julian Henao Marin <julianhm9612@gmail.com>
2024-05-12 12:03:45 -0500

    Fix cors

--- src/main/java/com/cinemas/neon/apigatewayserver/config/SecurityConfig.java
+++ src/main/java/com/cinemas/neon/apigatewayserver/config/SecurityConfig.java
@@ -1,11 +1,16 @@
 package com.cinemas.neon.apigatewayserver.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -20,6 +25,20 @@ public class SecurityConfig {
         ).oauth2ResourceServer((oauth) -> oauth
                 .jwt(Customizer.withDefaults()))
         .build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfiguration() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.applyPermitDefaultValues();
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+
+        UrlBasedCorsConfigurationSource source =
+            new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
     }
     
 }
